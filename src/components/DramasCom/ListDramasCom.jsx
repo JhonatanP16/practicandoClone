f20ee1694df04bd44bec14e176c0df4aa8e58036
JhonatanP16 <jhonatanpanduro16@gmail.com>
2023-08-31 13:37:38 -0500

    finish

--- src/components/DramasCom/ListDramasCom.jsx
+++ src/components/DramasCom/ListDramasCom.jsx
@@ -11,33 +11,33 @@ const ListDramasCom = ({dramas}) => {
       {
         dramas.map((drama,index) => (
             <div className={classes.contentCard} key={index}>
-            <Link
-            className={classes.cardLink}
-            style={{backgroundImage:`url(${drama.imgUrl})`}}
-            title={`${drama.title}`}
-            to={`/detalle/${(drama.titleUs).replace(/\s+/g, '-')}`}
-            state={drama}
-            >
-              {
-                pathname === '/favorites' && (
-                  <BsDownload className={classes.iconoDown} onClick={(e) => dowloadFile(e,drama.imgUrl)}/>
-                )
-              }
-            <div className={classes.play}>
-                <BsPlayCircle/>
+                <Link
+                className={classes.cardLink}
+                style={{backgroundImage:`url(${drama.imgUrl})`}}
+                title={`${drama.title}`}
+                to={`/detalle/${(drama.titleUs).replace(/\s+/g, '-')}`}
+                state={drama}
+                >
+                  {
+                    pathname === '/favorites' && (
+                      <BsDownload className={classes.iconoDown} onClick={(e) => dowloadFile(e,drama.imgUrl)}/>
+                    )
+                  }
+                <div className={classes.play}>
+                    <BsPlayCircle/>
+                </div>
+                <div className={classes.ribon}>
+                    <p>{drama.status}</p>
+                </div>
+                <div className={classes.calificacion}>
+                    <p>8.2</p><BsStarFill/>
+                </div>
+                </Link>
+                <div className={classes.textDrama}>
+                    <span>{drama.title}</span>
+                    <span>{drama.titleUs}</span>
+                </div>
             </div>
-            <div className={classes.ribon}>
-                <p>{drama.status}</p>
-            </div>
-            <div className={classes.calificacion}>
-                <p>8.2</p><BsStarFill/>
-            </div>
-            </Link>
-            <div className={classes.textDrama}>
-                <span>{drama.title}</span>
-                <span>{drama.titleUs}</span>
-            </div>
-      </div>
         ))
       }
     </div>
